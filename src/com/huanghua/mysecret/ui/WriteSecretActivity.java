4d9ac727dcea7c1bc068a218995e1784f4807f31
huanghua <huanghua@innovatech.net.cn>
2014-08-27 07:59:21 -0400

    添加发表图片

--- src/com/huanghua/mysecret/ui/WriteSecretActivity.java
+++ src/com/huanghua/mysecret/ui/WriteSecretActivity.java
@@ -1,11 +1,20 @@
 package com.huanghua.mysecret.ui;
 
+import java.io.File;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -13,7 +22,9 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import cn.bmob.v3.datatype.BmobFile;
 import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UploadFileListener;
 
 import com.huanghua.mysecret.R;
 import com.huanghua.mysecret.bean.Comment;
@@ -37,6 +48,7 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
     public static final int WRITE_TYPE_COMMENT = 2;
     public static final int WRITE_TYPE_REPLY_COMMENT = 3;
     public static final int WRITE_TYPE_PIC_SECRET = 4;
+    public static final float PIC_SCALE_WIDTH = 60;
 
     private int mWriteType = WRITE_TYPE_SECRET;
     private TextView mContentsCountView = null;
@@ -53,6 +65,8 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
     private ImageView mUserPhoto = null;
     private int mRandomHead = 0;
     private ImageView mAddPic = null;
+    private Bitmap mPicBitmap = null;
+    private String mPicFilePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,11 +146,11 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
         if (checkUserLogin()) {
             return;
         }
-        String content = mContents.getText().toString();
+        final String content = mContents.getText().toString();
         if (content != null && !"".endsWith(content)) {
             final Dialog dialog = CommonUtils.createLoadingDialog(this, getString(R.string.cominting));
             dialog.show();
-            if (mWriteType == WRITE_TYPE_SECRET || mWriteType == WRITE_TYPE_PIC_SECRET) {
+            if (mWriteType == WRITE_TYPE_SECRET) {
                 Secret s = new Secret();
                 s.setContents(content);
                 s.setUser(userManager.getCurrentUser());
@@ -208,6 +222,54 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
                         v.setClickable(true);
                     }
                 });
+            } else if (mWriteType == WRITE_TYPE_PIC_SECRET) {
+                File f = new File(mPicFilePath);
+                final BmobFile bmobFile = new BmobFile(f);
+                bmobFile.uploadblock(this, new UploadFileListener() {
+                    @Override
+                    public void onSuccess() {
+                        Secret s = new Secret();
+                        s.setContents(content);
+                        s.setUser(userManager.getCurrentUser());
+                        s.setStatus(0);
+                        s.setPic(mPicBitmap == null ? null : bmobFile.getFileUrl());
+                        s.setLocation(mShowLocation ? mLutil.findLocation() : null);
+                        s.setAddress(mLocation);
+                        s.setRandomHead(mRandomHead);
+                        s.save(WriteSecretActivity.this, new SaveListener() {
+                            @Override
+                            public void onSuccess() {
+                                showLog("save secret success ");
+                                dialog.dismiss();
+                                ShowToastOld(R.string.publication_success);
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        finish();
+                                    }
+                                }, 100);
+                            }
+
+                            @Override
+                            public void onFailure(int arg0, String arg1) {
+                                showLog("save secret failure " + arg1);
+                                dialog.dismiss();
+                                ShowToast(R.string.publication_faile);
+                                v.setClickable(true);
+                            }
+                        });
+                    }
+                    @Override
+                    public void onProgress(Integer arg0) {
+                    }
+                    @Override
+                    public void onFailure(int arg0, String arg1) {
+                        showLog("save secret failure " + arg1);
+                        dialog.dismiss();
+                        ShowToast(R.string.publication_faile);
+                        v.setClickable(true);
+                    }
+                });
             } else {
                 final User user = (User) getIntent().getSerializableExtra("toUser");
                 Secret secret = (Secret) getIntent().getSerializableExtra(
@@ -258,7 +320,7 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
 
     public void onBackPressed(){
         String content = mContents.getText().toString();
-        if (content != null && content.length() > 0) {
+        if ((content != null && content.length() > 0) || mPicBitmap != null) {
             showExitConfirmDialog();
         } else {
             super.onBackPressed();
@@ -299,7 +361,114 @@ public class WriteSecretActivity extends BaseActivity implements TextWatcher, Vi
     @Override
     public void onClick(View v) {
         if (v == mAddPic) {
-            
+            showSelectPicDialog();
         }
+    }
+
+    private void showSelectPicDialog() {
+        AlertDialog.Builder build = new AlertDialog.Builder(this)
+                .setTitle(R.string.add_pic_dialog_title).setNegativeButton(android.R.string.cancel, null)
+                .setCancelable(false);
+
+        if (mPicBitmap != null) {
+            build.setItems(R.array.add_pic_delete,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                            case 0:
+                                break;
+                            case 1:
+                                Intent intent = new Intent(
+                                        Intent.ACTION_GET_CONTENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                                intent.setType("image/*");
+                                startActivityForResult(intent, 1);
+                                break;
+                            case 2:
+                                mAddPic.setImageResource(R.drawable.add_pic);
+                                mPicBitmap.isRecycled();
+                                mPicBitmap = null;
+                                break;
+                            }
+                        }
+                    });
+        } else {
+            build.setItems(R.array.add_pic,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                            case 0:
+                                break;
+                            case 1:
+                                Intent intent = new Intent(
+                                        Intent.ACTION_GET_CONTENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                                intent.setType("image/*");
+                                startActivityForResult(intent, 1);
+                                break;
+                            }
+                        }
+                    });
+        }
+        build.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPicBitmap != null && !mPicBitmap.isRecycled()) {
+            mPicBitmap.recycle();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int arg0, int arg1, Intent arg2) {
+        super.onActivityResult(arg0, arg1, arg2);
+        if (arg0 == 1) {
+            if (arg1 == RESULT_OK) {
+                int sdk_int = Build.VERSION.SDK_INT;
+                if (sdk_int < 19) {
+
+                } else {
+
+                }
+                Uri imageUri = arg2.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                Cursor cursor = getContentResolver().query(imageUri,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                if (mPicBitmap != null && !mPicBitmap.isRecycled()) {
+                    mPicBitmap.isRecycled();
+                    mPicBitmap = null;
+                    mPicFilePath = null;
+                }
+                mPicFilePath = picturePath;
+                mPicBitmap = BitmapFactory.decodeFile(picturePath);
+                mAddPic.setImageBitmap(small(mPicBitmap, PIC_SCALE_WIDTH,
+                        PIC_SCALE_WIDTH));
+
+            }
+        }
+    }
+
+    private Bitmap small(Bitmap bitmap, float scaleW, float scaleH) {
+
+        int oldW = bitmap.getWidth();
+        int oldH = bitmap.getHeight();
+        float newW = scaleW;
+        float newH = scaleH;
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(newW / oldW, newH / oldH);
+        Bitmap resizeBmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                bitmap.getHeight(), matrix, true);
+        return resizeBmp;
     }
 }
